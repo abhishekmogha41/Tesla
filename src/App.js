81de2dcd422948a164370dafcd4ee88c1e6affb7
abhishekmogha41 <57898407+abhishekmogha41@users.noreply.github.com>
2023-01-19 12:24:43 +0530

    Image working!

--- src/App.js
+++ src/App.js
@@ -8,50 +8,50 @@ function App() {
       name: "Model 3",
       para: "Schedule a Demo Drive",
       leftbtn: "Custom Order",
-      rightBtn: "Existing Inventory",
-      background: "../../../public/images/model-3.jpg",
+      rightbtn: "Existing Inventory",
+      background: "images/model-3.jpg",
       arrow: true,
     },
     {
       name: "Model Y",
       para: "Schedule a Demo Drive",
       leftbtn: "Custom Order",
-      rightBtn: "Existing Inventory",
-      background: "../../../public/images/model-y.jpg",
+      rightbtn: "Existing Inventory",
+      background: "images/model-y.jpg",
     },
     {
       name: "Model S",
       para: "Schedule a Demo Drive",
       leftbtn: "Custom Order",
-      rightBtn: "Existing Inventory",
-      background: "../../../public/images/model-s.jpg",
+      rightbtn: "Existing Inventory",
+      background: "images/model-s.jpg",
     },
     {
       name: "Model X",
       para: "Schedule a Demo Drive",
       leftbtn: "Custom Order",
-      rightBtn: "Existing Inventory",
-      background: "../../../public/images/model-x.jpg",
+      rightbtn: "Existing Inventory",
+      background: "images/model-x.jpg",
     },
     {
       name: "Solar panels",
       para: "Lowest Cost Solar Panels in America",
       leftbtn: "Custom Order",
-      rightBtn: "Existing Inventory",
-      background: "../../../public/images/solar-panel.jpg",
+      rightbtn: "Existing Inventory",
+      background: "images/solar-panel.jpg",
     },
     {
       name: "Solar Roof",
       para: "Produce Clean Energy From Your Roof",
       leftbtn: "Custom Order",
-      rightBtn: "Existing Inventory",
-      background: "../../../public/images/solar-roof.jpg",
+      rightbtn: "Existing Inventory",
+      background: "images/solar-roof.jpg",
     },
     {
       name: "Accessories",
       para: "",
       leftbtn: "Custom Order",
-      background: "../../../public/images/accessories.jpg",
+      background: "images/accessories.jpg",
     },
   ];
   return (
@@ -61,49 +61,49 @@ function App() {
         name={data[0].name}
         para={data[0].para}
         leftbtn={data[0].leftbtn}
-        rightBtn={data[0].rightBtn}
+        rightbtn={data[0].rightbtn}
         background={data[0].background}
       ></Body>
       <Body
         name={data[1].name}
         para={data[1].para}
         leftbtn={data[1].leftbtn}
-        rightBtn={data[1].rightBtn}
+        rightbtn={data[1].rightbtn}
         background={data[1].background}
       ></Body>
       <Body
         name={data[2].name}
         para={data[2].para}
         leftbtn={data[2].leftbtn}
-        rightBtn={data[2].rightBtn}
+        rightbtn={data[2].rightbtn}
         background={data[2].background}
       ></Body>
       <Body
         name={data[3].name}
         para={data[3].para}
         leftbtn={data[3].leftbtn}
-        rightBtn={data[3].rightBtn}
+        rightbtn={data[3].rightbtn}
         background={data[3].background}
       ></Body>
       <Body
         name={data[4].name}
         para={data[4].para}
         leftbtn={data[4].leftbtn}
-        rightBtn={data[4].rightBtn}
+        rightbtn={data[4].rightbtn}
         background={data[4].background}
       ></Body>
       <Body
         name={data[5].name}
         para={data[5].para}
         leftbtn={data[5].leftbtn}
-        rightBtn={data[5].rightBtn}
+        rightbtn={data[5].rightbtn}
         background={data[5].background}
       ></Body>
       <Body
         name={data[6].name}
         para={data[6].para}
         leftbtn={data[6].leftbtn}
-        rightBtn={data[6].rightBtn}
+        rightbtn={data[6].rightbtn}
         background={data[6].background}
       ></Body>
     </>
--- src/Component/Body/Body.js
+++ src/Component/Body/Body.js
@@ -4,33 +4,42 @@ import "./Body.css";
 function body(props) {
   return (
     <>
-      <section>
-        <div
-        // style={{
-        //   backgroundImage: `url(${props.background})`,
-        // }}
-        >
-          <div className="carName">
-            <img src={props.background} alt="" />
-            <h1>{props.name}</h1>
-            <a href="/">
-              <p className="section_para"> {props.para}</p>
-            </a>
-          </div>
+      <section
+        style={{
+          backgroundImage: `url(${props.background})`,
+          // backgroundPosition: `center`,
+        }}
+      >
+        <div className="carName">
+          <h1>{props.name}</h1>
+          <a href="/">
+            <p className="section_para"> {props.para}</p>
+          </a>
+        </div>
 
-          <div className="buttons mb-2">
-            <div className="button-group">
-              <a href="/">{props.leftbtn}</a>
-              {props.rightBtn && <a href="/">{props.rightbtn}</a>}
-            </div>
+        <div className="buttons mb-2">
+          <div className="button-group">
+            <a href="/">
+              {" "}
+              <button>{props.leftbtn}</button>
+            </a>
+
+            {props.rightbtn ? (
+              <a href="/">
+                {" "}
+                <button>{props.rightbtn}</button>
+              </a>
+            ) : (
+              ""
+            )}
           </div>
-          <div>
-            <img
-              className="down-arrow"
-              src="images/down-arrow.svg"
-              alt="down-arrow"
-            />
-          </div>
+        </div>
+        <div>
+          <img
+            className="down-arrow"
+            src="images/down-arrow.svg"
+            alt="down-arrow"
+          />
         </div>
       </section>
     </>
